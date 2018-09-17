f15739bdd605e74e60496fd4d8587a090fdcb2e8
macbook <lucero.angel24@gmail.com>
2018-09-16 20:43:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,58 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+
+    private String mLocation;
+
+    private String mDate;
+
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    /** Website URL of the earthquake */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the city location of the earthquake
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *  earthquake happened
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public Earthquake() {
+    }
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
+
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+}
